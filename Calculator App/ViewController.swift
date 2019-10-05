fd21d4526fc8229e6506e16f5387a16ee8aefbaa
Nisha Tandel <angeltandel64.nt@gmail.com>
2019-10-04 21:37:15 -0400

    functionality updated.

--- Calculator App/ViewController.swift
+++ Calculator App/ViewController.swift
@@ -5,13 +5,21 @@
 //  Created by Nisha Tandel on 2019-09-25.
 //  Copyright © 2019 Nisha Tandel. All rights reserved.
 //
+// Assignment 1 : Calculator App
+// NAME : Nisha Jitendrakumar Tandel
+// Student Id : 301094688
+// Date : 4th October 2019
+// Version History : Added functionlity for button.
+                    // operator (+, -, %, *, ., /, backspace,clear)
+// Description : Final updated application with                        mathametical functions.
 
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // This will display the result of the opertions.
     @IBOutlet weak var resultlable: UILabel!
     
+    // defined variables
     var currentNumber: Double = 0
     var previousNumber: Double = 0
     var operation = 0
@@ -19,13 +27,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         resultlable.text = ""
     }
-    
+    // this function will be executed when the numbers are pressed.
     @IBAction func NumberPressed(_ sender: Any)
     {
-
+// "let method" will let function to work when pressed UI button
         let tag = (sender as! UIButton).tag
         
         if isPerformingOperation == true
@@ -41,12 +49,12 @@ class ViewController: UIViewController {
         }
         
     }
-    
+    // This function will execute when the operator will be pressed.s
     @IBAction func operatorPressed(_ sender: Any) {
         
         let tag = (sender as! UIButton).tag
         
-        /* clear button - 16 */
+        /* clear button - tag 16 */
         if tag == 16{
             resultlable.text = ""
             previousNumber = 0
@@ -56,9 +64,9 @@ class ViewController: UIViewController {
             return
         }
         
-        /* operation ÷(11) ×(12) -(13) +(14) =(15) */
+        /* operation(its tag) ÷(11) ×(12) -(13) +(14) =(15) .(19) %(18)*/
         
-        if tag == 11
+        if tag == 11 // division
         {
             isPerformingOperation = true
             previousNumber = Double(resultlable.text!)!
@@ -66,7 +74,7 @@ class ViewController: UIViewController {
             operation = tag
             
         }
-        else if tag == 12
+        else if tag == 12 //Multiplication
         {
             isPerformingOperation = true
             previousNumber = Double(resultlable.text!)!
@@ -74,7 +82,7 @@ class ViewController: UIViewController {
             operation = tag
             
         }
-        else if tag == 13
+        else if tag == 13 // Subtraction
         {
             isPerformingOperation = true
             previousNumber = Double(resultlable.text!)!
@@ -82,7 +90,7 @@ class ViewController: UIViewController {
             operation = tag
             
         }
-        else if tag == 14
+        else if tag == 14 // addition
         {
             isPerformingOperation = true
             previousNumber = Double(resultlable.text!)!
@@ -96,12 +104,14 @@ class ViewController: UIViewController {
             {
                 resultlable.text = resultlable.text! + "."
                 currentNumber = Double(resultlable.text!)!
-                //isDecimal = true
+
             }
         }
         else if tag == 18 // percentage
         {
-            //resultlable.text = String(Double(resultlable.text!))
+             isPerformingOperation = true
+            resultlable.text = String(Double(resultlable.text!)!/100)
+            
         }
             
         else if tag == 15
@@ -130,13 +140,15 @@ class ViewController: UIViewController {
         }
         
 }
+    // when backspace is pressed this function is called.
     @IBAction func backspaceButton(_ sender: Any)
     {
         let tag = (sender as! UIButton).tag
         
         /*  drop the last number from the resultlable*/
+        // 20 is the tag for backspace button
         
-        if tag == 20
+        if tag == 20 //backspace
         {
             let num = resultlable.text!.dropLast()
             currentNumber = Double(num) ?? 0.0
